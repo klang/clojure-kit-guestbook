6c877234b2b4508a82a64f4b7c1d4cc3c6a13ad8
Karsten Lang Pedersen <karstenlang@gmail.com>
2024-04-12 08:45:54 +0200

    initial

---
+++ env/dev/clj/klang/guestbook/dev_middleware.clj
@@ -0,0 +1,5 @@
+(ns klang.guestbook.dev-middleware)
+
+(defn wrap-dev [handler _opts]
+  (-> handler
+      ))
---
+++ test/clj/klang/guestbook/core_test.clj
@@ -0,0 +1,8 @@
+(ns klang.guestbook.core-test
+  (:require
+    [klang.guestbook.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+
